17e2d90ed9c1650418bfbdb7df2802c4ada123b8
Nate Dayton <natedayton2020@gmail.com>
2026-03-23 21:39:14 -0600

    JQuery move code
    Figured out how to get images to move and change to a different image on move. Have to adjust the code for each button later. Meant to commit earlier today

--- assignments/a1- playground/scripts.js
+++ assignments/a1- playground/scripts.js
@@ -1,8 +1,25 @@
 // import { animate } from 'https://esm.sh/animejs';
 
-// $(function () {
-//     $("left-button").on(`click`, function () {
-//         $(`falcon-gif`).animate()
+$(function () {
+    $("#left-button").on(`click`, function () {
+        $(`#falcon-gif`).animate({ "marginLeft": "-500px" }, 500).animate({ "marginLeft": "0px" }, 1500, "swing", function () {
+            $(`#falcon-gif`).attr("src", "assets/falcon-center.gif")
+        })
+        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+    })
 
-//     })
-// })
+    $("#up-button").on(`click`, function () {
+        $(`#falcon-gif`).animate({ "marginTop": "-500px" }, 500)
+        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+    })
+
+    $("#right-button").on(`click`, function () {
+        $(`#falcon-gif`).animate({ "marginRight": "-500px" }, 500)
+        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+    })
+
+    $("#down-button").on(`click`, function () {
+        $(`#falcon-gif`).animate({ "marginBottom": "-500px" }, 500)
+        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+    })
+})
