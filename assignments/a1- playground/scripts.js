175ac8d944ed7c1a2a290a624378c59215be2281
Nate Dayton <natedayton2020@gmail.com>
2026-03-25 09:50:46 -0600

    Added Trivia animate.css functionality
    added functionality with animate.css. made buttons for the trivia section and added some jquery functionality that adds animate.css classes to the buttons when clicked. will add a bit more styling and refine the design, still need to add hint.css functionality

--- assignments/a1- playground/scripts.js
+++ assignments/a1- playground/scripts.js
@@ -9,17 +9,33 @@ $(function () {
     })
 
     $("#up-button").on(`click`, function () {
-        $(`#falcon-gif`).animate({ "marginTop": "-500px" }, 500)
-        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+        $(`#falcon-gif`).animate({ "marginTop": "-250px" }, 500).animate({ "marginTop": "0px" }, 1500, "swing", function () {
+            $(`#falcon-gif`).attr("src", "assets/falcon-center.gif")
+        })
+        $(`#falcon-gif`).attr("src", "assets/falcon-up.gif")
     })
 
     $("#right-button").on(`click`, function () {
-        $(`#falcon-gif`).animate({ "marginRight": "-500px" }, 500)
-        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+        $(`#falcon-gif`).animate({ "marginRight": "-500px" }, 500).animate({ "marginRight": "0px" }, 1500, "swing", function () {
+            $(`#falcon-gif`).attr("src", "assets/falcon-center.gif")
+        })
+        $(`#falcon-gif`).attr("src", "assets/falcon-right.gif")
     })
 
     $("#down-button").on(`click`, function () {
-        $(`#falcon-gif`).animate({ "marginBottom": "-500px" }, 500)
-        $(`#falcon-gif`).attr("src", "assets/falcon-left.gif")
+        $(`#falcon-gif`).animate({ "marginBottom": "-250px" }, 500).animate({ "marginBottom": "0px" }, 1500, "swing", function () {
+            $(`#falcon-gif`).attr("src", "assets/falcon-center.gif")
+        })
+        $(`#falcon-gif`).attr("src", "assets/falcon-down.gif")
+    })
+
+    $(".wrong-answer").on(`click`, function(){
+        $(this).addClass('animate__animated animate__fadeOut')
+    })
+
+    $('#correct-answer').on(`click`, function(){
+        $(this).addClass('animate__animated animate__tada')
+        $(this).css('color', 'green')
     })
 })
+
