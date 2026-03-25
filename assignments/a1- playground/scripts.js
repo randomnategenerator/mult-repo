b3afd8f2092391838192314a1264340c9864c150
Nate Dayton <natedayton2020@gmail.com>
2026-03-25 13:00:34 -0600

    experimenting with animations for sections 3 and 4
    Experimented with jquery animations for sections 3 & 4, I know have my idea in mind for them, but will have to implement them next time I work on this. Currently the DVD bounces all around section 3, but I will make it bounce around the entire screen instead of just section 3

--- assignments/a1- playground/scripts.js
+++ assignments/a1- playground/scripts.js
@@ -29,13 +29,39 @@ $(function () {
         $(`#falcon-gif`).attr("src", "assets/falcon-down.gif")
     })
 
-    $(".wrong-answer").on(`click`, function(){
+
+
+    $(".wrong-answer").on(`click`, function () {
         $(this).addClass('animate__animated animate__fadeOut')
+        $(this).css('background-color', 'red')
     })
 
-    $('#correct-answer').on(`click`, function(){
+    $('#correct-answer').on(`click`, function () {
         $(this).addClass('animate__animated animate__tada')
-        $(this).css('color', 'green')
+        $(this).css('background-color', 'lime')
     })
+
+    //figure out how to make this work like i want
+
+    $(window).on("scroll", function () {
+       $('#dvd').animate({
+            "marginLeft": `+${1 + Math.floor(Math.random() * window.innerWidth)}`,
+            "marginTop": `+${1 + Math.floor(Math.random() * window.innerHeight)}`,
+            "marginRight": `+${1 + Math.floor(Math.random() * window.innerWidth)}`,
+            "marginBottom": `+${1 + Math.floor(Math.random() * window.innerHeight)}`
+        })
+
+        console.log($(`scroll-activated-image`).css('height'))
+    })
+
+
+    // $('#choose-your-own-adventure').on('mousemove', function () {
+    //     $('#dvd').animate({
+    //         "marginLeft": `+${1 + Math.floor(Math.random() * window.innerWidth)}`,
+    //         "marginTop": `+${1 + Math.floor(Math.random() * window.innerHeight)}`,
+    //         "marginRight": `+${1 + Math.floor(Math.random() * window.innerWidth)}`,
+    //         "marginBottom": `+${1 + Math.floor(Math.random() * window.innerHeight)}`
+    //     })
+    // })
 })
 
