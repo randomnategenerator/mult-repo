d7c11d57b6afaba74f4d554c994efa1b7ef8a287
Nate Dayton <141164969+randomnategenerator@users.noreply.github.com>
2026-04-20 11:37:25 -0600

    added footer and cards
    footer and cards implemented by referring to Bootstrap docs, and some help with positioning and padding from AI. currently working on implementing my scroll effect how i want

--- assignments/a3-website/scripts.js
+++ assignments/a3-website/scripts.js
@@ -1,11 +1,12 @@
 gsap.to('#profileScroll', {
-	scrollTrigger: '#profileScroll', // start the animation when ".box" enters the viewport (once)
-	y: -500
+	scrollTrigger: '#profileScroll', 
+	scrub: true,
+	y: "-=50"
 });
 
 const myCarouselElement = document.querySelector('#carouselExampleCaptions')
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  touch: true
+	interval: 2000,
+	touch: true
 })
