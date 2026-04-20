11f7fbd787114f0dcbebfdc653c466c6e887d0f3
Nate Dayton <141164969+randomnategenerator@users.noreply.github.com>
2026-04-20 10:12:06 -0600

    added carousel implementation
    using help from bootstrap documentation and ai, i implemented a carousel for featured projects. going to add cards for the "planet" semester links.

--- assignments/a3-website/scripts.js
+++ assignments/a3-website/scripts.js
@@ -1,2 +1,11 @@
-let controller = new ScrollMagic.Controller();
-let scene = new ScrollMagic.Scene();
+gsap.to('#profileScroll', {
+	scrollTrigger: '#profileScroll', // start the animation when ".box" enters the viewport (once)
+	y: -500
+});
+
+const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: true
+})
