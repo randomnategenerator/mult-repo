e66f5b6456b5086dfaf7a84d332c4b66b130910b
Nate Dayton <141164969+randomnategenerator@users.noreply.github.com>
2026-04-22 10:20:35 -0600

    Added semester 4 page
    Started the design for the semester 4 page, which will create a template for the rest of the semester pages

--- assignments/a3-website/scripts.js
+++ assignments/a3-website/scripts.js
@@ -1,8 +1,20 @@
-gsap.to('#profileScroll', {
-	scrollTrigger: '#profileScroll', 
-	scrub: true,
-	y: "-=50"
+// gsap.to('#profileScroll', {
+// 	scrollTrigger: '#profileScroll', 
+// 	scrub: true,
+// 	y: "-=50"
+// });
+
+gsap.set("#spaceBackground", {xPercent: -50});
+
+let xGo = gsap.quickTo("#spaceBackground", "x", {duration: 0.5, ease:"power3"})
+
+let hoverSection = document.getElementById("profileSwipe")
+
+hoverSection.addEventListener("mousemove", e => {
+  xGo(e.clientX);
 });
+
+
 
 const myCarouselElement = document.querySelector('#carouselExampleCaptions')
 
